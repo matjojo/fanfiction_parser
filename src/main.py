403d2120996bd024e44fea45e5f3903486dec03c
matjojo <mat2904jojo@gmail.com>
2019-11-23 16:27:47 +0100

    Moves urlLib parsing to inside the ao3 parser block, as ffnet does not support it

--- src/main.py
+++ src/main.py
@@ -6,14 +6,14 @@ import parsers.fanfiction_net as fanfiction_net
 
 def main():
 	url = input("What ffnet or ao3 gallery to parse? (any filters in the url will be left as they are): ")
-	url = url.replace("https://", "").replace("http://", "")
-	url = "https://" + urllib.parse.quote(url)
+	url = "https://" + url.replace("https://", "").replace("http://", "")  # ensure the link is https
 	links = []
 	if "fanfiction.net" in url:
 		print("fanfiction.net link detected")
 		links = fanfiction_net.get_links(url)
 	elif "archiveofourown.org" in url:
 		print("archiveofourown.org link detected")
+		url = "https://" + urllib.parse.quote(url)  # not all sites support this (ffnet grmbl)
 		links = archive_of_our_own_org.get_links(url)
 
 	with open("links.txt", "a+") as file:
